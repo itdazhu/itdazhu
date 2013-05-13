533d438cd885ab7942ddc39f3fb16f9bd4f4a9bc
itdazhu <itdazhu@gmail.com>
2013-05-13 10:42:58 +0800

    Update stu.java

--- stu.java
+++ stu.java
@@ -1,5 +1,12 @@
 class stu{
 	private int age;
+	private String name;
+	public void setName(String name){
+		this.name = name;
+	}
+	public String getName(){
+		return this.name;
+	}
 	public void setAge(int age){
 		this.age = age ;
 	}
